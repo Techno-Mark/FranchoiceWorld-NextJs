c3745b931d8d8b374b9c8e4515e293ea84d4e81b
PACIFICABS\bhargav.lakhani <bhargav.lakhani@technomark.io>
2024-07-12 12:33:04 +0530

    Links section added in Investor page

--- src/app/investor/page.tsx
+++ src/app/investor/page.tsx
@@ -2,6 +2,7 @@
 import Faq from "@/components/faq/faq";
 import FindFranchise from "@/components/findFranchise/findFranchise";
 import InnerListBrandBanner from "@/components/innerListBrandBanner/innerListBrandBanner";
+import QuickLinks from "@/components/quickLinks/quickLinks";
 import Testimonial from "@/components/testimonial/testimonial";
 import TopBrandSlider from "@/components/topBrands/topBrands";
 import TrandingVideo from "@/components/trandingVideo/trandingVideo";
@@ -187,6 +188,100 @@ const whatWeDo = {
   ],
 };
 
+const quickLinksData = [
+  {
+    title: "Browse By Investment Range",
+    linkItem: [
+      { content: "Under 1 Lakh", path: "#" },
+      { content: "Under 2 Lakh", path: "#" },
+      { content: "Under 3 Lakh", path: "#" },
+      { content: "Under 5 Lakh", path: "#" },
+      { content: "Under 10 Lakh", path: "#" },
+      { content: "Under 15 Lakh", path: "#" },
+      { content: "Under 20 Lakh", path: "#" },
+      { content: "Under 25 Lakh", path: "#" },
+      { content: "Under 30 Lakh", path: "#" },
+      { content: "Under 50 Lakh", path: "#" },
+      { content: "Under 1 Crore", path: "#" },
+    ],
+  },
+  {
+    title: "Browse By Location",
+    linkItem: [
+      { content: "Maharashtra", path: "#" },
+      { content: "Delhi", path: "#" },
+      { content: "Karnataka", path: "#" },
+      { content: "Tamil Nadu", path: "#" },
+      { content: "West Bengal", path: "#" },
+      { content: "Gujarat", path: "#" },
+      { content: "Uttar Pradesh", path: "#" },
+      { content: "Madhya Pradesh", path: "#" },
+      { content: "Haryana", path: "#" },
+      { content: "Rajasthan", path: "#" },
+      { content: "Andhra Pradesh", path: "#" },
+      { content: "Kerala", path: "#" },
+      { content: "Punjab", path: "#" },
+      { content: "Chandigarh", path: "#" },
+    ],
+  },
+  {
+    title: "Popular Cities",
+    linkItem: [
+      { content: "Agra", path: "#" },
+      { content: "Ahmedabad", path: "#" },
+      { content: "Amritsar", path: "#" },
+      { content: "Aurngabad", path: "#" },
+      { content: "Bengaluru", path: "#" },
+      { content: "Bhopal", path: "#" },
+      { content: "Bhubaneswar", path: "#" },
+      { content: "Chandigarh", path: "#" },
+      { content: "Chennai", path: "#" },
+      { content: "Coimbatore", path: "#" },
+      { content: "Dehradun", path: "#" },
+      { content: "Faridabad", path: "#" },
+      { content: "Ghaziabad", path: "#" },
+      { content: "Gurugram", path: "#" },
+      { content: "Guwahati", path: "#" },
+      { content: "Hyderabad", path: "#" },
+      { content: "Indore", path: "#" },
+      { content: "Jabalpur", path: "#" },
+      { content: "Jaipur", path: "#" },
+      { content: "Jamshedpur", path: "#" },
+      { content: "Jodhpur", path: "#" },
+      { content: "Kanpur", path: "#" },
+      { content: "Kochi", path: "#" },
+      { content: "Kolkata", path: "#" },
+      { content: "Kota", path: "#" },
+      { content: "Lucknow", path: "#" },
+      { content: "Ludhiana", path: "#" },
+      { content: "Madurai", path: "#" },
+      { content: "Mangalore", path: "#" },
+      { content: "Meerut", path: "#" },
+      { content: "Mumbai", path: "#" },
+      { content: "Mysuru", path: "#" },
+      { content: "Nagpur", path: "#" },
+      { content: "Nashik", path: "#" },
+      { content: "Navi Mumbai", path: "#" },
+      { content: "New Delhi", path: "#" },
+      { content: "Noida", path: "#" },
+      { content: "Patna", path: "#" },
+      { content: "Prayagraj", path: "#" },
+      { content: "Pune", path: "#" },
+      { content: "Raipur", path: "#" },
+      { content: "Rajkot", path: "#" },
+      { content: "Ranchi", path: "#" },
+      { content: "Shimla", path: "#" },
+      { content: "Surat", path: "#" },
+      { content: "Thiruvananthapuram", path: "#" },
+      { content: "Udaipur", path: "#" },
+      { content: "Vadodara", path: "#" },
+      { content: "Varanasi", path: "#" },
+      { content: "Vijayawada", path: "#" },
+      { content: "Visakhapatnam", path: "#" },
+    ],
+  },
+];
+
 const Investor = () => {
   const innerBanner = {
     bannerImage: "/images/innerInvestorBanner.png",
@@ -219,6 +314,7 @@ const Investor = () => {
         items={whatWeDo.items}
       />
       <Faq />
+      <QuickLinks quickLink={quickLinksData} />
     </>
   );
 };
