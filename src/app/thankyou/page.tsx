d4a8499c62b6db8aa1dc8f0051ece986b905657b
PACIFICABS\bhargav.lakhani <bhargav.lakhani@technomark.io>
2024-07-18 13:33:38 +0530

    fixed thank you page

--- src/app/thankyou/page.tsx
+++ src/app/thankyou/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Button from "@/components/button/button";
+import Title from "@/components/title/title";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
@@ -15,17 +16,27 @@ const ThankYou = () => {
   const icon = "/images/thanks/thankyou.gif";
 
   return (
-    <div className="w-full min-h-screen flex flex-col justify-center items-center">
-      <div className="mb-8">
-        <Image src={icon} alt={"Thank you"} width={133} height={133} />
-      </div>
-      <div className="font-bold text-3xl  text-[rgba(23,73,138,1)]">
-        Thank you for registering your brand with us!
-      </div>
-      <div className="mb-20 text-base font-medium text-[rgba(115,114,115,1)]">
-        You&apos;re all set.Our team will contact you soon!
-      </div>
-      <div>
+    <div className="py-16 text-center">
+      <div className="container">
+        <div className="mb-6 md:mb-8">
+          <Image
+            className="mx-auto w-[91px] h-[91px] md:w-[133px] md:h-[133px]"
+            src={icon}
+            alt={"Thank you"}
+            width={133}
+            height={133}
+          />
+        </div>
+        <Title
+          title="Thank you for registering your brand with us!"
+          titleClass="!pb-4 !text-2xl"
+        />
+        <div className="mb-10 text-base font-medium text-[rgba(115,114,115,1)]">
+          You&apos;re all set.{" "}
+          <span className="block md:inline-block">
+            Our team will contact you soon!
+          </span>
+        </div>
         <Button variant="highlighted" type="button" onClick={handleClick}>
           Back to home page
         </Button>
