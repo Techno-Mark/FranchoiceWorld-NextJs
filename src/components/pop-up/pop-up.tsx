bd4bacf563644f0c55888152fc14f7cd319a6abf
Alvish6654 <alvish.pipaliya@technomark.io>
2024-09-19 12:37:27 +0530

    time change in pop up

--- src/components/pop-up/pop-up.tsx
+++ src/components/pop-up/pop-up.tsx
@@ -62,7 +62,7 @@ const MainPopup = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowConsent(true);
-    }, 150);
+    }, 15000);
 
     return () => clearTimeout(timer);
   }, []);
