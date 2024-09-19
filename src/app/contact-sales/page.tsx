19010c2e4178a1b2dd66629aa474f72f4e6fc09b
Alvish6654 <alvish.pipaliya@technomark.io>
2024-09-19 17:44:23 +0530

    address updated

--- src/app/contact-sales/page.tsx
+++ src/app/contact-sales/page.tsx
@@ -6,7 +6,7 @@ const ContactSales = () => {
   return (
     <>
       <ContactBanner pageFroms="Contact Us"/>
-      <GetInTouch location="Ahmedabad | Mumbai"/>
+      <GetInTouch locations={["Ahmedabad: 3rd Floor, 305, Circle P, Sarkhej Gandhinagar Road, Opp. Panchavati Auto, 100 Foot Road, Ahmedabad, Gujarat, 380015", "Mumbai: Office No. 4, 4th Floor, K Raheja Prime, Marol CHS Rd, Marol, Andheri East, Mumbai, Maharashtra, 400059"]} />
     </>
   );
 };
